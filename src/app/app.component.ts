a6051809f75d266633458779a2ca1421af57a6fe
reivaj93 <jav.alonso0493@gmail.com>
2024-04-22 03:50:44 -0600

    First Version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import * as data from '../assets/api.json';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule, ReactiveFormsModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'CodeChallengeExpeditors';
+  
+  private apiURL = '/assets/api.json'; // La URL debería ser relativa a la raíz del servidor
+  users: any[] = [];
+
+  
+  constructor(private http: HttpClient) {
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.http.get<any[]>(this.apiURL).subscribe((res) => {
+      console.log(res);
+      this.users = res;
+    })
+  }
+  
+
 }
